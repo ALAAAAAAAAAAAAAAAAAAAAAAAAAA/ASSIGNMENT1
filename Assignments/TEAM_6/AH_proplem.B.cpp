03392c1a039a5d77b228c0ae3afe463db903d78f
ALAAAAAAAAAAAAAAAAAAAAAAAAAA <141523751+ALAAAAAAAAAAAAAAAAAAAAAAAAAA@users.noreply.github.com>
2023-08-05 23:36:28 +0300

    Create AH_proplem.B.cpp

---
+++ Assignments/TEAM_6/AH_proplem.B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   string str;
+   for(int i=0;i<t;i++){
+        cin>>str;
+          reverse(str.begin(),str.end());
+   cout<<str<<endl;
+   }
+
+}
+//alaa hassan
