92ed3469f8a921823feec495847ecb58ecf03721
ALAAAAAAAAAAAAAAAAAAAAAAAAAA <141523751+ALAAAAAAAAAAAAAAAAAAAAAAAAAA@users.noreply.github.com>
2023-08-05 23:31:09 +0300

    Create AH_proplem_A.cpp

---
+++ Assignments/TEAM_6/AH_proplem_A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int t,x,n;
+    cin>>t;
+    for(int i=0; i<t; i++){
+            cin>>x>>n;
+      cout<<  pow(x,n);
+    }
+    return 0;
+}
+//alaa hassan
